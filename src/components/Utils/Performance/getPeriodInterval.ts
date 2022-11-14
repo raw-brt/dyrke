acf81c93eda8f05d817c2b602d2a9cdca1605e8b
Roberto <robertodzbt@gmail.com>
2022-11-14 20:42:26 +0100

    feat(performance): period helper WIP

---
+++ src/components/Utils/Performance/getPeriodInterval.ts
@@ -0,0 +1,38 @@
+import dayjs  from "dayjs";
+/**
+ * @desc Returns a time interval in unix time to filter queries
+ * @param period desired time interval selected in chart
+ * @returns interval in unix format
+ */
+
+type Period = "Year" | "90 days" | "30 days" | "Week" | "Today";
+type TimeInterval = { start: number, end: number };
+
+export const getPeriodInterval = (period: Period): TimeInterval => {
+  if (period === "Year") {
+    return {
+      start: dayjs().unix(), 
+      end: dayjs().subtract(1, "year").unix()
+    };
+  } else if (period === "90 days") {
+    return {
+      start: dayjs().unix(), 
+      end: dayjs().subtract(90, "day").unix()
+    };
+  } else if (period === "30 days") {
+    return {
+      start: dayjs().unix(), 
+      end: dayjs().subtract(30, "day").unix()
+    };
+  } else if (period === "Week") {
+    return {
+      start: dayjs().unix(), 
+      end: dayjs().subtract(7, "day").unix()
+    };
+  } else {
+    return {
+      start: dayjs().unix(), 
+      end: dayjs().subtract(1, "day").unix()
+    };
+  }
+};
--- src/routes/Performance/Performance.tsx
+++ src/routes/Performance/Performance.tsx
@@ -5,6 +5,7 @@ import { Metric, Period } from "@generated/dyrketypes";
 import { Header } from "@components/Performance/Header";
 import { useProfileStore } from "src/store/profiles";
 import { Chart } from "@components/Performance/Chart";
+import { getPeriodInterval } from "@components/Utils/Performance/getPeriodInterval";
 
 const sdk = getBuiltGraphSDK();
 
@@ -19,6 +20,9 @@ export const Performance: FC = () => {
   const result = useQuery(["TestQuery"], () => sdk.TestQuery(), { refetchOnWindowFocus: false });
 
   console.log(result)
+
+  const testHelper = getPeriodInterval("Year");
+  console.log(testHelper)
 
   return (
     <section className="w-auto h-full mx-10 my-8 flex flex-col justify-start items-start space-y-10">
