2233370b18aa0e50ca9410c1f9530e6e480223f3
Roberto <robertodzbt@gmail.com>
2022-11-19 18:45:04 +0100

    feat(performance): profile infinite query wip

--- src/components/Profile/Feed.tsx
+++ src/components/Profile/Feed.tsx
@@ -20,7 +20,6 @@ import { Card } from "../UI/Card";
 import { SinglePublication } from "../Publication/SinglePublication";
 import { DyrkePublication } from "src/generated/dyrketypes";
 import { getAuthProperties } from "@lib/getFetchOptions";
-import { useQueryClient } from "@tanstack/react-query";
 
 interface Props {
   profile: Profile;
@@ -29,7 +28,6 @@ interface Props {
 
 export const Feed: FC<Props> = ({ profile, type }) => {
   const [cursor, setCursor] = useState(null);
-  const [feedPublications, setFeedPublications] = useState<unknown[]>([]);
   const accessToken = useAuthStore((state) => state.authState.accessToken);
   const currentProfile = useProfileStore((state) => state.currentProfile);
 
@@ -62,21 +60,6 @@ export const Feed: FC<Props> = ({ profile, type }) => {
   const reactionRequest = currentProfile ? { profileId: currentProfile?.id } : null;
   const profileId = currentProfile?.id ?? null;
 
-  // const profileFeed = useProfileFeedQuery(
-  //   getAuthProperties(MAINNET_API_URL, accessToken),
-  //   {
-  //     request,
-  //     reactionRequest,
-  //     profileId,
-  //   },
-  //   {
-  //     refetchOnWindowFocus: false,
-  //     keepPreviousData: true,
-  //     // Al estar haciendo el spread, cuando cambio el tipo de feed me los acumula, no los actualiza
-  //     // onSuccess: (data) => setFeedPublications([...feedPublications as [any], data?.publications?.items])
-  //   },
-  // );
-
   const profileFeed = useProfileFeedInfiniteQuery(
     getAuthProperties(MAINNET_API_URL, accessToken),
     {
@@ -94,44 +77,29 @@ export const Feed: FC<Props> = ({ profile, type }) => {
     },
   );
 
-  const handleLoadMore = () => {
-    setCursor(profileFeed?.data?.pages[profileFeed?.data?.pages.length - 1]?.publications?.pageInfo?.next)
-  };
-
-  // const publications = profileFeed?.data?.publications?.items;
-  // const feedLength = profileFeed?.data?.pages.reduce((acc: number, page: any) => acc + page?.publications?.items.length as number);
-
-  // const loadMore = () => {
-  //   console.log("Entra load more")
-  //   if (hasMore) setCursor(profileFeed?.data?.publications?.pageInfo?.next);
-  // };
-
-  // if (profileFeed.isFetching) return <PublicationsSkeleton />;
-
-  // if (publications?.length === 0) {
-  //   return (
-  //     <Empty
-  //       message={
-  //         <div>
-  //           <span className='mr-1 font-bold'>@{profile?.handle}</span>
-  //           <span>doesn’t {type.toLowerCase()}ed yet!</span>
-  //         </div>
-  //       }
-  //       icon={<RectangleStackIcon className='w-8 h-8 text-primary-500' />}
-  //     />
-  //   );
-  // }
-
   if (profileFeed.error) {
     return <ErrorMessage title='Failed to load profile feed' error={profileFeed?.error} />;
   }
 
+  const handleLoadMore = () => {
+    profileFeed.fetchNextPage();
+    setCursor(profileFeed?.data?.pages[profileFeed?.data?.pages.length - 1]?.publications?.pageInfo?.next);
+  };
+
+  const handlePublicationsLength = () => {
+    let counter = 0;
+    profileFeed?.data?.pages?.map((page) => counter += page.publications.items.length)
+    return counter;
+  };
+
+  if (profileFeed.isSuccess && profileFeed?.data.pages) console.log(profileFeed?.data.pages)
+
   return (
     <div className='w-full h-full pb-10 overflow-auto' id='scrollableDiv'>
       <InfiniteScroll
-        dataLength={profileFeed?.data?.pages?.length ?? 0}
+        dataLength={handlePublicationsLength() ?? 0}
         scrollThreshold={SCROLL_THRESHOLD}
-        hasMore={!!profileFeed.hasNextPage}
+        hasMore={profileFeed.hasNextPage === true ? true : false}
         next={handleLoadMore}
         loader={<InfiniteLoader />}
         scrollableTarget="scrollableDiv"
